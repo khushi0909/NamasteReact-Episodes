2601ee7e6e61f270561cf65fa1e7ee5be89e33da
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-07-04 22:33:30 +0200

    lazy loading impleted for Grocery comp

--- Episode9/OptimisingOurApp/src/App.js
+++ Episode9/OptimisingOurApp/src/App.js
@@ -1,5 +1,6 @@
 
 import React from "react"
+import { lazy,Suspense } from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
@@ -8,10 +9,10 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Error from "./components/Error"
 import RestaurantDetails from "./components/RestaurantDetails"
-import Grocery from "./components/Grocery"
+// import Grocery from "./components/Grocery"
 
 
-
+const Grocery = lazy(()=> import ("./components/Grocery"))
         
 const AppLayout = () => {
     return (
@@ -41,7 +42,7 @@ const AppLayout = () => {
                  },
                  {        
                     path:"/grocery",
-                    element:<Grocery/>,
+                    element:<Suspense fallback={<h1>Loading....</h1>}><Grocery/> </Suspense>,
      
              },
                  {        
