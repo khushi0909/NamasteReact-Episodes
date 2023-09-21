01589d789dc35696cd958089e7188f014e23d469
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-09-21 23:44:54 +0200

    describe is used to group similar test cases and it and test word are same

--- Episode13/timefortest/src/components/__tests__/Contact.test.js
+++ Episode13/timefortest/src/components/__tests__/Contact.test.js
@@ -3,43 +3,49 @@ import {render,screen} from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom"
 
-test("should load contact us component ",() => {
+//describe is the grouping of similar test cases 
 
-
-render(<Contact/>)
-const heading = screen.getByRole("heading")
-
-//Assertion
-expect(heading).toBeInTheDocument();
-
-
-
-})
-
-test("should load button us component ",() => {
-
-
-    render(<Contact/>)
-    const button = screen.getByRole("button")
-    
-    //Assertion
-    expect(button).toBeInTheDocument();
-    
-    
-    
-    })
-    test("should load 2 input boxes on the contact component  ",() => {
+describe("Contact us Page Test Cases ",() => {
+    test("should load contact us component ",() => {
 
 
         render(<Contact/>)
-
-        //Querying
-        const inputBoxes = screen.getAllByRole("textbox")
-        console.log(inputBoxes.length)
+        const heading = screen.getByRole("heading")
         
         //Assertion
-        expect(inputBoxes.length).toBe(2);
+        expect(heading).toBeInTheDocument();
         
         
         
         })
+        
+
+        //instead of test you can write it also 
+        it("should load button us component ",() => {
+        
+        
+            render(<Contact/>)
+            const button = screen.getByRole("button")
+            
+            //Assertion
+            expect(button).toBeInTheDocument();
+            
+            
+            
+            })
+            test("should load 2 input boxes on the contact component  ",() => {
+        
+        
+                render(<Contact/>)
+        
+                //Querying
+                const inputBoxes = screen.getAllByRole("textbox")
+                console.log(inputBoxes.length)
+                
+                //Assertion
+                expect(inputBoxes.length).toBe(2);
+                
+                
+                
+                })
+})
